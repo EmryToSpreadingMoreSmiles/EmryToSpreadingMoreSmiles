7032743b95c7b7fcb36b84cfdec7271332dc7864
Chirag Nagpal <85030123+chiragnagpal02@users.noreply.github.com>
2024-01-21 16:59:14 +0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,9 @@ function App() {
       <Routes>
           {/* Other Routes */}
           <Route path='/EmryToSpreadingMoreSmiles/' element={<Home />} />
-          <Route path='/mission' element={<Mission />} />
-          <Route path='/work' element={<Work />} />
-          <Route path='/gallery' element={<Gallery />} />
+          <Route path='/EmryToSpreadingMoreSmiles/mission' element={<Mission />} />
+          <Route path='/EmryToSpreadingMoreSmiles/work' element={<Work />} />
+          <Route path='/EmryToSpreadingMoreSmiles/gallery' element={<Gallery />} />
           <Route path='https://emrytospreadingmoresmiles.github.io/EmryToSpreadingMoreSmiles/footer' element={<Footer />} />
           <Route path='https://emrytospreadingmoresmiles.github.io/EmryToSpreadingMoreSmiles/contact' element={<ContactUs />} />
 
