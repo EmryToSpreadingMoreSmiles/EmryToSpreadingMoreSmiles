6e2ec966f97c3ad57723da4d760aa84b13c084f8
Chirag Nagpal <85030123+chiragnagpal02@users.noreply.github.com>
2024-01-21 18:16:06 +0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,36 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Work from './components/Work'
-import Gallery from './components/Gallery'
-import Footer from './components/Footer'
-import Mission from './components/Mission'
-import ContactUs from './components/ContactUs'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Work from "./components/Work";
+import Gallery from "./components/Gallery";
+import Footer from "./components/Footer";
+import Mission from "./components/Mission";
+import ContactUs from "./components/ContactUs";
 
-import { motion } from 'framer-motion'
-import { styles } from './styles'
+import { motion } from "framer-motion";
+import { styles } from "./styles";
 
-import { HashRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
-
-
-
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
 
 function App() {
-
   return (
-    <BrowserRouter basename='/EmryToSpreadingMoreSmiles'>
+    <BrowserRouter basename="/EmryToSpreadingMoreSmiles">
       <Routes>
-          {/* Other Routes */}
-          <Route path='/' element={<Home />} />
-          <Route path='/mission' element={<Mission />} />
-          <Route path='/work' element={<Work />} />
-          <Route path='/gallery' element={<Gallery />} />
-          <Route path='/footer' element={<Footer />} />
-          <Route path='/contact' element={<ContactUs />} />
-
-        </Routes>
-
-
-        </BrowserRouter>
-
-   
-        
-       
-      
-    
-  )
+        {/* Other Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/mission" element={<Mission />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
